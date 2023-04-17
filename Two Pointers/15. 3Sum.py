fb80aa1672d49304562a7e53630dc0db4dfc005a
Olivia Xu <oliviaxu0528@Gmail.com>
2023-04-17 11:42:21 -0700

    3sum

--- Two Pointers/15. 3Sum.py
+++ Two Pointers/15. 3Sum.py
@@ -4,7 +4,6 @@
 
 # Notice that the solution set must not contain duplicate triplets.
 
-
 # Example 1:
 
 # Input: nums = [-1,0,1,2,-1,-4]
@@ -25,3 +24,30 @@
 # Input: nums = [0,0,0]
 # Output: [[0,0,0]]
 # Explanation: The only possible triplet sums up to 0.
+def threeSum(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    # sort the nums
+    nums.sort()
+    print(nums)
+    # take the first one, loop through the rest as a two sum
+    res = []
+    for i in range(len(nums)-2):
+        left = i+1
+        right = len(nums)-1
+        while left < right:
+            if nums[left]+nums[right] == -nums[i]:
+                res.append([nums[left], nums[right], nums[i]])
+            elif nums[left]+nums[right] + nums[i] < 0:
+                left += 1
+            else:
+                right -= 1
+
+    # print("res: ": res)
+    return res
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+print(threeSum(nums))
