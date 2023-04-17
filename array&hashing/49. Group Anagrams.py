7e9ca63978896183dcf66ea160c290fb12d43bad
Olivia Xu <oliviaxu0528@Gmail.com>
2023-04-16 20:48:56 -0700

    Update 49. Group Anagrams.py

--- array&hashing/49. Group Anagrams.py
+++ array&hashing/49. Group Anagrams.py
@@ -1,29 +1,35 @@
-#4.12.2023
+# 4.12.2023
 import collections
 
 # method1
-def groupAnagrams(strs):
-  ans = collections.defaultdict(list)
+# def groupAnagrams(strs):
+#   ans = collections.defaultdict(list)
 
-  for s in strs:
-    count = [0]*26
-    for c in s:
-      count[ord(c)-ord("a")]+=1
-    # print("count: ",count)
-    ans[tuple(count)].append(s)
-    print("ans: ", ans)
+#   for s in strs:
+#     count = [0]*26
+#     for c in s:
+#       count[ord(c)-ord("a")]+=1
+#     # print("count: ",count)
+#     ans[tuple(count)].append(s)
+#     print("ans: ", ans)
 
-  return ans.values()
+#   return ans.values()
 
 # method2
+
+
 def groupAnagrams(strs):
-  hashmap = {}
-  for str in strs:
-      str1 = ''.join(sorted(str))
-      if str1 not in hashmap:
-          hashmap[str1] = []
-      hashmap[str1].append(str)
+    hashmap = {}
+    for str in strs:
+        print("str: ", str)
+        str1 = ''.join(sorted(str))  # pure sorted look like ['a', 'e', 't']
+        print("str1: ", str1)
+        if str1 not in hashmap:
+            hashmap[str1] = []
+        hashmap[str1].append(str)
 
-  return list(hashmap.values())
-print(groupAnagrams(["eat","tea","tae","abc"]))
+    return list(hashmap.values())
 
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(groupAnagrams(strs))
