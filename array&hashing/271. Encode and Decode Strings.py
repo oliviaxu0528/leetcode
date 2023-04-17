2df70129b1234bef501c130459e33bbf7fcee5df
Olivia Xu <oliviaxu0528@Gmail.com>
2023-04-16 20:21:39 -0700

    271

--- array&hashing/271. Encode and Decode Strings.py
+++ array&hashing/271. Encode and Decode Strings.py
@@ -4,16 +4,32 @@ def encode(strs):
     :type strs: List[str]
     :rtype: str
     """
+    s = ""
+    for string in strs:
+        s += str(len(string))+"#" + string
+    return s
 
-    return ''.join(strs)
 
-
+y = encode(["Hello", "World"])
 print(encode(["Hello", "World"]))
 
 
-# # def decode(s):
-#     """Decodes a single string to a list of strings.
+def decode(s):
+    """Decodes a single string to a list of strings.
 
-#     :type s: str
-#     :rtype: List[str]
-#     """
+    :type s: str
+    :rtype: List[str]
+    """
+    res, i = [], 0
+    while i < len(s):
+        j = i
+        while s[j] != "#":
+            j += 1
+            length = int(s[i:j])
+        char = s[j+1:j+1+length]
+        res.append(char)
+        i = j+1+length
+    return res
+
+
+print(decode(y))
