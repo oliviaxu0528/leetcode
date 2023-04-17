209eed8463e1d2a4dc8cf8d41eb2ea894895cf01
Olivia Xu <oliviaxu0528@Gmail.com>
2023-04-16 18:31:44 -0700

    adj

---
+++ array&hashing/271. Encode and Decode Strings.py
@@ -0,0 +1,19 @@
+def encode(strs):
+    """Encodes a list of strings to a single string.
+
+    :type strs: List[str]
+    :rtype: str
+    """
+
+    return ''.join(strs)
+
+
+print(encode(["Hello", "World"]))
+
+
+# # def decode(s):
+#     """Decodes a single string to a list of strings.
+
+#     :type s: str
+#     :rtype: List[str]
+#     """
