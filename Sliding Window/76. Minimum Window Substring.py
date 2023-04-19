829c72deb3a75141817027f6f9f0f9e3a5838e88
Olivia Xu <110135215+oliviaxu0528@users.noreply.github.com>
2023-04-18 17:06:33 -0700

    Update 76. Minimum Window Substring.py

--- Sliding Window/76. Minimum Window Substring.py
+++ Sliding Window/76. Minimum Window Substring.py
@@ -21,3 +21,33 @@
 # Output: ""
 # Explanation: Both 'a's from t must be included in the window.
 # Since the largest window of s only has one 'a', return empty string.
+def minWindow(s,t):
+  #create countT
+  countT = {}
+  for c in t:
+      countT[c] = 1+countT.get(c,0)
+  #create sliding window {}
+  window = {}
+  l = 0
+  res, resLen = [-1,-1],float("inf")
+  have, need = 0, len(countT)
+  for r in range(len(s)):
+      window[s[r]] = 1+ window.get(s[r],0)
+      if s[r] in countT and window[s[r]] == countT[s[r]]:
+          have +=1
+      while have == need:
+          #update the result
+          if (r-l+1) <resLen:
+              res = [l, r]
+              resLen = r-l+1
+          #remove the left
+          window[s[l]] -=1
+          if s[l] in countT and window[s[l]] < countT[s[l]]:
+              have -= 1
+          l+=1
+  (l, r) =res
+  return s[l : r + 1] if resLen != float("infinity") else ""
+
+s = "ADOBECODEBANC"
+t = "ABC"
+print(minWindow(s,t))
