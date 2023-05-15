3d6d64ec9f4e8b0299f8433284aa842093b77cee
Olivia Xu <oliviaxu0528@Gmail.com>
2023-05-15 09:48:09 -0700

    lc42

---
+++ Array&Hashing/42. Trapping Rain Water.py
@@ -0,0 +1,15 @@
+def trap(height):
+    l, r = 0, len(height)-1
+    leftMax, rightMax = height[l], height[r]
+    res = 0
+
+    while l < r:
+        if leftMax < rightMax:
+            l += 1
+            leftMax = max(leftMax, height[l])
+            res += leftMax-height[l]
+        else:
+            r -= 1
+            rightMax = max(rightMax, height[r])
+            res += rightMax-height[r]
+    return res
