2ff1b80e50867d81ed1a6b25a1c8175ff6f9b809
Olivia Xu <110135215+oliviaxu0528@users.noreply.github.com>
2023-05-24 10:02:47 -0700

    lc 739

---
+++ Stack/739. Daily Temperatures.py
@@ -0,0 +1,9 @@
+# Daily Temperatures - Monotonic Stack - Leetcode 739 - Python
+def dailyTemperatures(temperatures):
+    l = len(temperatures)
+    for i in range(l):
+        for j in range(0, j)
+
+
+temperatures = [73,74,75,71,69,72,76,73]
+dailyTemperatures(temperatures)
