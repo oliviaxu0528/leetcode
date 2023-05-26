60c9a7a4ddf672ef9d1df403e6434803b2bd852c
Olivia Xu <110135215+oliviaxu0528@users.noreply.github.com>
2023-05-26 15:15:36 -0700

    567

--- Sliding Window/567. Permutation in String.py
+++ Sliding Window/567. Permutation in String.py
@@ -3,31 +3,25 @@ def checkInclusion(s1,s2):
     cntr, w, match = Counter(s1),len(s1),0
     for i in range(len(s2)):
         if s2[i] in cntr:
+            # print("match1: ", match)
             if not cntr[s2[i]]: match -=1
-            print("i1:", i)
-            print("match1:", match)
+            
+            print("cntr: ",cntr)
             cntr[s2[i]] -=1
+            print("cntr2: ",cntr)
             if not cntr[s2[i]]: match +=1
-            print("i2:", i)
-            print("match2:", match)
-            print("cntr111: ", cntr)   
 
         if i>=w and s2[i-w] in cntr:
             if not cntr[s2[i-w]]: match -=1
-            print("i3:", i)
-            print("match3:", match)
             cntr[s2[i-w]] +=1
             if not cntr[s2[i-w]]: match +=1
-            print("i4:", i)
-            print("match4:", match)
 
         if match == len(cntr):
-            print("cntr2222: ", cntr)
             return True
     return False
 
-s1 = "abc"
-s2 = "caddbcc"
+s1 = "ab"
+s2 = "cadba"
 print(checkInclusion(s1,s2))
                 
     
